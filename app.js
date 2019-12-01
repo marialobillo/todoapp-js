fe24e54a4b36d6e0c1f5995160742db5f2bb4f23
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2019-12-01 20:32:56 +0100

    saving a new todo and showing it

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
+document.getElementById('todoForm').addEventListener('submit', saveTodo);
 
+// initial array of todos
+const todos = [];
+
+function saveTodo(e){
+    e.preventDefault();
+
+    let title = document.getElementById('title').value;
+    
+    // validation
+    if(title.length > 0){
+        const todo = {
+            title, 
+            complete: false, 
+            id: todos.length > 0 ? todos[todos.length -1].id + 1 : 1,
+        }
+
+        todos.push(todo);
+    }
+
+    document.getElementById('todoForm').reset();
+    showTodos();
+}
+
+function showTodos(){
+
+    const todoList = document.getElementById('todoList');
+
+    todoList.innerHTML = '';
+
+    for(let i = 0; i < todos.length; i++){
+        console.log(todos[i].title);
+    }
+}
